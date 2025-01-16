9b81a3501fd946f1af66e5938b9dfff08914cd82
Aum Bhatt <aum@deriv.com>
2025-01-16 11:31:14 +0400

    feat: added custom loader

--- src/components/prompt-field/prompt-field.tsx
+++ src/components/prompt-field/prompt-field.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { InputGroupButton } from '@deriv-com/quill-ui';
+import { InputGroupButton, SectionMessage, Spinner } from '@deriv-com/quill-ui';
 import { useGenerateStrategy } from './useGenerateStrategy';
 import './prompt-field.scss';
 
@@ -14,26 +14,38 @@ export const PromptField: React.FC = () => {
 
     return (
         <div className='prompt-field-wrapper'>
-            <InputGroupButton
-                buttonLabel='Generate'
-                button_position='right'
-                disabled={isLoading}
-                inputSize='md'
-                leftIcon={
-                    <img
-                        style={{ width: '20px', height: '20px' }}
-                        src='https://media.nngroup.com/media/editor/2024/09/16/figma_generate_sparkles_ai_icon.png'
-                    />
-                }
-                message=''
-                placeholder='What parameters do you need for strategy?'
-                status='neutral'
-                type='text'
-                variant='fill'
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)}
-                buttonCallback={handleGenerate}
-            />
+            {!isLoading ? (
+                <InputGroupButton
+                    buttonLabel='Generate'
+                    button_position='right'
+                    disabled={isLoading}
+                    inputSize='md'
+                    leftIcon={
+                        <img
+                            style={{ width: '20px', height: '20px' }}
+                            src='https://media.nngroup.com/media/editor/2024/09/16/figma_generate_sparkles_ai_icon.png'
+                        />
+                    }
+                    message=''
+                    placeholder='What parameters do you need for strategy?'
+                    status='neutral'
+                    type='text'
+                    variant='fill'
+                    value={inputValue}
+                    onChange={e => setInputValue(e.target.value)}
+                    buttonCallback={handleGenerate}
+                />
+            ) : (
+                <SectionMessage
+                    className='prompt-field__loader'
+                    icon={<Spinner />}
+                    linkList={null}
+                    message='Generating strategy'
+                    size='sm'
+                    status='info'
+                    title=''
+                />
+            )}
         </div>
     );
 };
